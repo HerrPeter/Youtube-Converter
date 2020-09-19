66c5c33021e4f6c991169ee17ff34b8e13f704eb
Peter Kaufinger <peterkaufinger@gmail.com>
2020-09-19 12:20:08 -0700

    Added ytdl lib

--- src/app/convert.service.ts
+++ src/app/convert.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
+
+import { createWriteStream } from 'fs';
+import ytdl from 'ytdl-core';
+
+// const fs = require('fs');
+// const ytdl = require('ytdl-core');
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConvertService {
-  constructor(private http: HttpClient) {}
+  // constructor(private http: HttpClient) {}
 
-  toMp3(url: string) {
-    let id: string = this.getVideoId(url);
+  toMp3() {
+    ytdl('https://www.youtube.com/watch?v=tDTQQWSmo8s').pipe(
+      createWriteStream('Video.flv')
+    );
 
-    return this.http.get(`https://www.yt-download.org/api/button/mp3/${id}`);
+    // let id: string = this.getVideoId(url);
+
+    // return this.http.get(`https://www.yt-download.org/api/button/mp3/${id}`);
   }
 
   getVideoId(url: string) {
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -10,12 +10,9 @@ export class HomePageComponent implements OnInit {
   constructor(private _http: ConvertService) {}
 
   ngOnInit(): void {
-    this._http
-      .toMp3(
-        'https://www.youtube.com/watch?v=9x1MZEDQbtA&list=PLmPDdtpUSi9Pp5Fx842q1FOaZjiV5WQGq&index=32&t=0s&ab_channel=BeatlesArchivesHQ'
-      )
-      .subscribe((data) => {
-        console.log(data);
-      });
+    this._http.toMp3();
+    // .subscribe((data) => {
+    //   console.log(data);
+    // });
   }
 }
