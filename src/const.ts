21ea0814959bda57b59dc28e7de1ba201c03987e
Peter Kaufinger <34880135+HerrPeter@users.noreply.github.com>
2020-10-04 01:44:32 -0700

    Changed origin server domain

---
+++ src/const.ts
@@ -0,0 +1,9 @@
+export const _SERVER = {
+  SSL_DOMAIN: 'https://7a688e7d0807.ngrok.io', // Need to replace localhost w/ host ip for production.
+  DOMAIN: ' http://7a688e7d0807.ngrok.io',
+  REQUESTS: {
+    SINGLE_DOWNLOAD: 'single',
+    PLAYLIST_DOWNLOAD: 'playlist',
+    VALIDATE_URL: 'validate',
+  },
+};
