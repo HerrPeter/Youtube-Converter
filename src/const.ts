d8a71ec0a6fd0f990659a6dd951081d6c1e2d29e
Peter Kaufinger <34880135+HerrPeter@users.noreply.github.com>
2020-10-09 14:55:00 -0700

    Comment change

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 export const _SERVER = {
-  SSL_DOMAIN: 'https://674a99513007.ngrok.io', // Need to replace localhost w/ host ip for production.
+  SSL_DOMAIN: 'https://674a99513007.ngrok.io', // Need to update when new domain is created from ngrok backend.
   DOMAIN: ' https://674a99513007.ngrok.io',
   REQUESTS: {
     SINGLE_DOWNLOAD: 'single',
