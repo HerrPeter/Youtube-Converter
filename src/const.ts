94d0d020a376792e161ba55567b72a2b66f49f41
Peter Kaufinger <34880135+HerrPeter@users.noreply.github.com>
2020-10-08 20:24:31 -0700

    Server domain change

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const _SERVER = {
-  SSL_DOMAIN: 'https://c3a1349565c3.ngrok.io', // Need to replace localhost w/ host ip for production.
-  DOMAIN: ' http://c3a1349565c3.ngrok.io',
+  SSL_DOMAIN: 'https://674a99513007.ngrok.io', // Need to replace localhost w/ host ip for production.
+  DOMAIN: ' https://674a99513007.ngrok.io',
   REQUESTS: {
     SINGLE_DOWNLOAD: 'single',
     PLAYLIST_DOWNLOAD: 'playlist',
