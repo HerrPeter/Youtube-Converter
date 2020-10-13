40de6a0fed3cb6c23182a4015778d763a5a89ac9
Peter Kaufinger <34880135+HerrPeter@users.noreply.github.com>
2020-10-13 02:30:50 -0700

    Changed server origin

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const _SERVER = {
-  SSL_DOMAIN: 'https://674a99513007.ngrok.io', // Need to update when new domain is created from ngrok backend.
-  DOMAIN: ' https://674a99513007.ngrok.io',
+  SSL_DOMAIN: 'http://4a85fe8dfb29.ngrok.io', // Need to update when new domain is created from ngrok backend.
+  DOMAIN: ' https://4a85fe8dfb29.ngrok.io',
   REQUESTS: {
     SINGLE_DOWNLOAD: 'single',
     PLAYLIST_DOWNLOAD: 'playlist',
