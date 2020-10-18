80b5683599952ade8bd3b5d2967d4e82e211bd29
Peter Kaufinger <34880135+HerrPeter@users.noreply.github.com>
2020-10-17 18:29:33 -0700

    Changed domain url to secure environment variable on vercel

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const _SERVER = {
-  SSL_DOMAIN: 'https://4a85fe8dfb29.ngrok.io', // Need to update when new domain is created from ngrok backend.
-  DOMAIN: ' http://4a85fe8dfb29.ngrok.io',
+  SSL_DOMAIN: process.env.SSL_SERVER, //'https://4a85fe8dfb29.ngrok.io', // Need to update when new domain is created from ngrok backend.
+  DOMAIN: process.env.SSL_SERVER,
   REQUESTS: {
     SINGLE_DOWNLOAD: 'single',
     PLAYLIST_DOWNLOAD: 'playlist',
