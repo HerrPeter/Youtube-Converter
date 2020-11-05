e032340e03928bdaf09c7830fcf174d2fdb1d426
Peter Kaufinger <34880135+HerrPeter@users.noreply.github.com>
2020-11-05 11:01:40 -0800

    Updated server domain

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const _SERVER = {
-  SSL_DOMAIN: 'https://b63a0ceb919f.ngrok.io', // Need to update when new domain is created from ngrok backend.
-  DOMAIN: 'https://b63a0ceb919f.ngrok.io',
+  SSL_DOMAIN: 'https://4eb5da909ad9.ngrok.io', // Need to update when new domain is created from ngrok backend.
+  DOMAIN: 'http://4eb5da909ad9.ngrok.io',
   REQUESTS: {
     SINGLE_DOWNLOAD: 'single',
     PLAYLIST_DOWNLOAD: 'playlist',
