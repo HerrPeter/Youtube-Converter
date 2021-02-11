34373fe31bd5c2cb4718b5102f033a7a43e5180f
Peter Kaufinger <34880135+HerrPeter@users.noreply.github.com>
2021-02-10 17:01:23 -0800

    Updated server domain to heroku domain

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const _SERVER = {
-  SSL_DOMAIN: 'https://be79e1f18386.ngrok.io', // Need to update when new domain is created from ngrok backend.
-  DOMAIN: 'http://be79e1f18386.ngrok.io',
+  SSL_DOMAIN: 'https://server-ytdl.herokuapp.com/', // Need to update when new domain is created from ngrok backend.
+  DOMAIN: 'https://server-ytdl.herokuapp.com/',
   REQUESTS: {
     SINGLE_DOWNLOAD: 'single',
     PLAYLIST_DOWNLOAD: 'playlist',
