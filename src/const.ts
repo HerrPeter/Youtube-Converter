8280f502ef8f274855002f526ac5935cc308c3e6
Peter Kaufinger <34880135+HerrPeter@users.noreply.github.com>
2023-04-07 21:00:33 -0700

    Silly little switch of domain and ssl_domain positions in const.ts

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const _SERVER = {
-  SSL_DOMAIN: 'http://localhost:8000', // Need to update when new domain is created from ngrok backend.
   DOMAIN: 'http://localhost:8000',
+  SSL_DOMAIN: 'http://localhost:8000', // Need to update when new domain is created from ngrok backend.
   REQUESTS: {
     PIC_TO_CALENDAR: 'pic-to-calendar',
     SINGLE_DOWNLOAD: 'single',
