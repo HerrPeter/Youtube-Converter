672cd48145358077973d4182d748bb5cecaf5588
Peter (PDT2) <peterkaufinger@gmail.com>
2024-02-28 21:29:31 -0800

    Replaced domain temp due to bandwidth limits

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const _SERVER = {
-  DOMAIN: 'http://monkfish-witty-unlikely.ngrok-free.app', //'https://server-yt-down.onrender.com',
-  SSL_DOMAIN: 'https://monkfish-witty-unlikely.ngrok-free.app', //'https://server-yt-down.onrender.com', //'https://server-ytdl.herokuapp.com', // Need to update when new domain is created from ngrok backend.
+  DOMAIN: 'http://192.168.50.3:8000', //'http://monkfish-witty-unlikely.ngrok-free.app' (use when bandwidth issue fixed), //'https://server-yt-down.onrender.com',
+  SSL_DOMAIN: 'https://192.168.50.3:8000', //'https://monkfish-witty-unlikely.ngrok-free.app', //'https://server-yt-down.onrender.com', //'https://server-ytdl.herokuapp.com', // Need to update when new domain is created from ngrok backend.
   REQUESTS: {
     PIC_TO_CALENDAR: 'pic-to-calendar',
     SINGLE_DOWNLOAD: 'single',
