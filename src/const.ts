3e6cd912739dd616d7e883aac7245107c22024dd
Peter (PDT2) <peterkaufinger@gmail.com>
2024-03-01 16:42:50 -0800

    Changed tunnel url to server

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const _SERVER = {
-  DOMAIN: 'http://monkfish-witty-unlikely.ngrok-free.app', //(use when bandwidth issue fixed), //'https://server-yt-down.onrender.com',
-  SSL_DOMAIN: 'https://monkfish-witty-unlikely.ngrok-free.app', //'https://server-yt-down.onrender.com', //'https://server-ytdl.herokuapp.com', // Need to update when new domain is created from ngrok backend.
+  DOMAIN: 'https://serious-settled-even-cleaning.trycloudflare.com', // ngrok: 'http://monkfish-witty-unlikely.ngrok-free.app', //(use when bandwidth issue fixed), //'https://server-yt-down.onrender.com',
+  SSL_DOMAIN: 'https://serious-settled-even-cleaning.trycloudflare.com', //'https://monkfish-witty-unlikely.ngrok-free.app', //'https://server-yt-down.onrender.com', //'https://server-ytdl.herokuapp.com', // Need to update when new domain is created from ngrok backend.
   REQUESTS: {
     PIC_TO_CALENDAR: 'pic-to-calendar',
     SINGLE_DOWNLOAD: 'single',
