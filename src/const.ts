175b899551e29545bc9649ce0583bbe4ee7dcced
Peter (PLT-2) <peterkaufinger@gmail.com>
2026-02-13 10:41:24 -0800

    Modified to work due to version incompatibilities

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const _SERVER = {
-  SSL_DOMAIN: 'https://9c44fab7ed3c.ngrok.io', // Need to update when new domain is created from ngrok backend.
-  DOMAIN: 'http://9c44fab7ed3c.ngrok.io',
+  SSL_DOMAIN: 'https://0f29-137-151-175-112.ngrok-free.app', //'https://9c44fab7ed3c.ngrok.io', // Need to update when new domain is created from ngrok backend.
+  DOMAIN: 'https://0f29-137-151-175-112.ngrok-free.app', //'http://9c44fab7ed3c.ngrok.io',
   REQUESTS: {
     SINGLE_DOWNLOAD: 'single',
     PLAYLIST_DOWNLOAD: 'playlist',
